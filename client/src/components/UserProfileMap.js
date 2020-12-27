87c7082b6b2c8e0e90fd62cd93537735be4e4fc0
Sagar Suri <sagar_13@live.com>
2020-12-26 19:46:07 -0500

    Fixed redirect issue and improved user profile responsiveness

--- client/src/components/UserProfileMap.js
+++ client/src/components/UserProfileMap.js
@@ -1,19 +1,9 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 
-const useStyles = makeStyles({
-    map: {
-        backgroundPosition: "center center",
-        backgroundSize: "cover",
-        height: "50%",
-    },
-});
-
-export default function UserProfileMap({ location }) {
-    const classes = useStyles();
+export default function UserProfileMap({ className, location }) {
     const backgroundImage = `url(/maps/static?center=${encodeURIComponent(
         location
     )})`;
-    return <Grid className={classes.map} item xs={12} style={{ backgroundImage }} />;
+    return <Grid className={className} item xs={12} style={{ backgroundImage }} />;
 }
